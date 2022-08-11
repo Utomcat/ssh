4b963d2e0255b8bd628c4823adafab50372e3692
刺客 <1390851149@qq.com>
2022-08-11 17:55:03 +0800

    fix(ssh): 增加力扣练习题

--- src/test/java/com/ranyikang/ssh/SshApplicationTests.java
+++ src/test/java/com/ranyikang/ssh/SshApplicationTests.java
@@ -470,4 +470,19 @@ class SshApplicationTests {
 
 
     }
+
+    /**
+     * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+     * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+     * <p>
+     * P   A   H   N <br/>
+     * A P L S I I G <br/>
+     * Y   I   R <br/>
+     * <p>
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+     */
+    @Test
+    void test18() {
+
+    }
 }
