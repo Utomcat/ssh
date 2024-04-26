1de513f93223d13ea1c044d22a56a935278e2536
ranyk <1390851149@qq.com>
2024-04-26 16:54:12 +0800

    fix(ssh): 增加图片的长宽和分辨率处理

--- src/main/java/com/ranyikang/ssh/api/TestApi.java
+++ src/main/java/com/ranyikang/ssh/api/TestApi.java
@@ -3,8 +3,16 @@ package com.ranyikang.ssh.api;
 import com.ranyikang.ssh.common.Response;
 import com.ranyikang.ssh.entity.Student;
 import com.ranyikang.ssh.service.TestService;
+import lombok.val;
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.Imaging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
 
 /**
  * CLASS_NAME: TestApi.java <br/>
@@ -13,11 +21,13 @@ import org.springframework.web.bind.annotation.*;
  * @version V1.0           <br/>
  * @description: 测试API 类  <br/>
  * @date: 2022-07-14 <br/>
+
  */
 @RestController
 @RequestMapping("test")
 public class TestApi {
 
+    private static final Logger log = LoggerFactory.getLogger(TestApi.class);
     /**
      * 测试业务处理对象
      */
@@ -55,9 +65,8 @@ public class TestApi {
     }
 
 
-
     @GetMapping("testJpaOntToMany")
-    public Response testJpaOntToMany(){
+    public Response testJpaOntToMany() {
         return Response.valueOfObject(testService.testJpaOneToMany());
     }
 
@@ -65,6 +74,23 @@ public class TestApi {
     @PostMapping("save")
     public Response save(@RequestBody Student student) {
         return Response.valueOfObject(testService.testSave(student));
+    }
+
+    @PostMapping("upload/image")
+    public Response uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            log.info("文件名: ==> {}", file.getOriginalFilename());
+            log.info("文件类型: ==> {}", file.getContentType());
+            val image = ImageIO.read(file.getInputStream());
+            log.info("文件长 ==> {}", image.getWidth());
+            log.info("文件宽 ==> {}", image.getHeight());
+            ImageInfo imageInfo = Imaging.getImageInfo(file.getInputStream(), file.getOriginalFilename());
+            log.info("文件水平DPI ==> {}", imageInfo.getPhysicalWidthDpi());
+            log.info("文件垂直DPI ==> {}", imageInfo.getPhysicalHeightDpi());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return Response.SUCCESS;
     }
 
 }
